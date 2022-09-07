053d879afa16db806e8b5596e609efe43331696f
Izabella Oliveira <izabellapoliv@gmail.com>
2022-09-07 18:56:45 -0300

    setup backend to save picture and return url

--- app/api/serializers/item.py
+++ app/api/serializers/item.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from api.models.item import Item
 
+
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    picture = serializers.ImageField(required=False)
+
     class Meta:
         model = Item
         fields = '__all__'
--- app/api/views/item.py
+++ app/api/views/item.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from api.serializers.item import ItemSerializer
 from api.models.item import Item
 from api.filters.item import ItemFilterSet
 
+
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all().order_by('date_found')
     serializer_class = ItemSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    parser_classes = (MultiPartParser, FormParser)
+
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
     filterset_class = ItemFilterSet
+
     search_fields = ['title']
     ordering_fields = ['title', 'date_found', 'date_delivered']
